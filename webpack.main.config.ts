27f68c7bdc3d5c1cae82d2fa6a6f348cdb439237
yiminwook <grs0416@gmail.com>
2023-12-06 17:05:33 +0900

    Init: react 18 with typescript

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,5 +1,5 @@
 import type { Configuration } from 'webpack';
-
+import path from 'path';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
@@ -8,7 +8,7 @@ export const mainConfig: Configuration = {
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/index.ts',
+  entry: './src/main.ts',
   // Put your normal webpack config below here
   module: {
     rules,
