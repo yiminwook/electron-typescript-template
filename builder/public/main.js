f0cc0fbd039e7110dd9addc0851ea07eb708a1c6
yiminwook <grs0416@gmail.com>
2023-12-07 19:32:27 +0900

    Update: electron-builder files setting

--- builder/public/main.js
+++ builder/public/main.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
-let mainWindow;
+let mainWindow = null;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -15,31 +15,39 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './index.html')}`);
 
   if (isDev) {
-    mainWindow.webContents.openDevTools({ mode: "detach" });
+    //개발자 도구를 분리해서 실행
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+
+    //콘솔로그를 터미널에 표시
+    mainWindow.webContents.on('console-message', (event, level, message, line, sourceId) => {
+      console.log(`${message} ${sourceId} (${line})`);
+    });
+
+    console.log('Electron app start!!!');
   }
 
   mainWindow.setResizable(true);
-  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null));
   mainWindow.focus();
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
+  }
+
+  if (isDev) {
+    console.log('Electron app closed!!!');
   }
 });
 
-app.on("activate", () => {
-  if (mainWindow === null) {
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
